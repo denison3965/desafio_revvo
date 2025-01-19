d3beb65f4040dfa00795b3e9cd7dd0ed23814cdb
denison <denison.portela@ambrait.com>
2025-01-19 02:18:04 -0300

    Making the project to connect with postgress successfuly and fiz autoload to a better name

---
+++ src/config/ConnectionDB.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\config;
+
+use PDO;
+
+class ConnectionDB {
+
+    private string $host = 'postgres-db';
+    private string $port = '5432';
+    private string $dbname = 'main';
+    private string $user = 'user';
+    private string $password = 'pass';
+
+
+    public function __construct() {
+        $dns_pdo = "pgsql:host=$this->host;port=$this->port;dbname=$this->dbname";
+        $pdo = new PDO($dns_pdo, $this->user, $this->password);
+        $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        echo "Conexão com o PostgreSQL realizada com sucesso!";
+    }
+}
---
+++ src/public/api/courses/create.php
@@ -0,0 +1,9 @@
+<?php 
+
+require_once "../../../../vendor/autoload.php";
+
+use App\config\ConnectionDB;
+
+echo 'ola mundo';
+
+$c = new ConnectionDB();
